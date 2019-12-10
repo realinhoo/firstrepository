d87249fa9f1e727003dbf043df322abd67c2fdcf
Your Name <you@example.com>
2019-12-10 18:13:10 +0500

    Добавили файлы

---
+++ lesson.py
@@ -0,0 +1,16 @@
+#n = int(input('Вводим с клавиатyры: '))
+
+#for i in range(1, n):
+#    print(f"{i}, ", end='')
+#print(n) 
+
+
+
+n = int(input('Введите количество: '))
+
+for i in range(n, 1, -1):
+    print(i)
+for i in range(1, n + 1, 1):                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+    print(i)
+
+
