ceb3c3eceea8dbd2bd3321ffa741cfac25318edb
Your Name <you@example.com>
2019-12-26 17:39:25 +0500

    Задачи

--- lesson.py
+++ lesson.py
@@ -1,16 +1,57 @@
-#n = int(input('Вводим с клавиатyры: '))
 
-#for i in range(1, n):
-#    print(f"{i}, ", end='')
-#print(n) 
-
-
-
-n = int(input('Введите количество: '))
-
-for i in range(n, 1, -1):
-    print(i)
-for i in range(1, n + 1, 1):                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-    print(i)
+#s = int(input('Ввести данные: '))
+#for i in range(s, 0, -1):
+#    for j in range(i):
+#        print('*', end='')
+#    print()
+#for i in range(2, s + 1, 1):
+#    for j in range(i):
+#        print('*', end='')
+#    print()
 
 
+n = int(input('Ввести данные: '))
+while n % 2 == 0:
+    print('Не корректно.')
+    break
+else:
+    print('Пойдет')
+
+for i in range(n, 0, -2):
+    for j in range( (n - i) // 2):
+        print(' ', end='')
+    for j in range(i):
+        print('*', end='')
+    print()
+
+for i in range(3, n + 1, 2):
+    for j in range( (n - i) // 2):
+        print(' ', end='')
+    for j in range(i):
+        print('*', end='')
+    print()
+
+
+
+
+
+
+
+
+
+
+#n = int(input('Данные: '))
+
+#for i in range(n):
+#    for j in range(n):
+#        if i >= j and (n - 1) <= (i + j) or i <= j and (n - 1) >= (i + j):
+#            print('*', end='')
+#        else:
+#            print('.', end='')
+#    print()
+# (n - 1) <= (i + j)
+# i <= j
+# i <= j and (n - 1) <= (i + j)
+# i >= j and (n - 1) <= (i + j)
+# i <= j and (n - 1) >= (i + j)
+#i >= j and (n - 1) >= (i + j) or i <= j and (n - 1) <= (i + j)
