4a8499f300d6e2d8de3bc3a5053fc1b219b072a4
Your Name <you@example.com>
2020-02-04 18:47:54 +0500

    Часики

--- clock.py
+++ clock.py
@@ -1,19 +1,39 @@
-def clock(x):
+def clock(x, s):
     for i in range(n, 0, -2):
-        for j in range((n-i) // 2):
+        for j in range((n-i) // 2 + s):
             print(' ', end='')
         for j in range(i):
             print('*', end='')
         print()
+
     for i in range(3, n + 1, 2):
-        for j in range((n-i) // 2):
+        for j in range((n-i) // 2 + s):
             print(' ', end='')
         for j in range(i):
             print('*', end='')
         print()
 
+n = int(input('Данные: '))
+s = int(input('Отступ: '))
+
+print(clock(n, s))
 
 
 
-n = int(input('Данные 1: '))
-print(clock(n))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
