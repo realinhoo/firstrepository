7386d2e869dd1579149e8c6b33c982ceb39f09ed
Your Name <you@example.com>
2020-02-11 19:03:36 +0500

    Lucky

--- ticket.py
+++ ticket.py
@@ -3,26 +3,24 @@ def lucky(x):
     for i in range(3):
         b1 = b1 + x % 10
         x = x // 10
-
     b2 = 0
     for i in range(3):
         b2 = b2 + x % 10
         x = x // 10
-    
+
     return b1 == b2
     
 ticket_number_start = int(input('Введите номер: '))
 ticket_number_end = int(input('Введите номер: '))
+count = 0
+for ticket_number in range(ticket_number_start, ticket_number_end + 1):
+    if lucky(ticket_number):
+        count += 1
 
-if lucky(ticket_number):
-    print("Счастливый")
-else:
-    print("Не счастливый")
+print(count)
 
-q = 0
-for i in range(ticket_number_start, ticket_number_end):
-    q += 1
-print(q)
+
+
 
 
 
