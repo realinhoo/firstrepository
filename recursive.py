1966d9ba53908a9305789da49a7138b3909fd397
Your Name <you@example.com>
2020-02-27 18:31:47 +0500

    Рекурсия

--- recursive.py
+++ recursive.py
@@ -1,8 +1,21 @@
-def func_fib(x):
-    if x == 1 or x == 2:
-        return 1
-    return func_fib(x - 1) + func_fib(x - 2)
+#def func_fib(x):
+#    if x == 1 or x == 2:
+#        return 1
+#    return func_fib(x - 1) + func_fib(x - 2)
+#n = int(input('Число: '))
+#fib = func_fib(n)
+#print(fib)
 
-n = int(input('Число: '))
-fib = func_fib(n)
-print(fib)
+# def factorial(x):
+#     if x == 0:
+#         return 1
+#     return x * factorial(x - 1)
+# number = int(input('Число: '))
+# print(factorial(number))
+
+def number(x):
+    if x == 0:
+        return
+
+
+user = int(input('Число: '))
